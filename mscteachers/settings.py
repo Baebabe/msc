3e9b79a58fb3d38c30bb10b9d57f8396d8c49c6f
Baebabe <baibhavs45@gmail.com>
2025-01-27 13:21:31 +0545

    Initial commit4

--- mscteachers/settings.py
+++ mscteachers/settings.py
@@ -21,16 +21,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'n=7ud-_go7@qv=-dmpdgp@84c)%b-f6vty1y5%w5ngi3_^aw_s'
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://mscpw.onrender.com',
-    'http://mscpw.onrender.com'
-]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CSRF_COOKIE_SECURE = True  # for HTTPS
-SECURE_SSL_REDIRECT = True  # to redirect HTTP to HTTPS
+# Security settings
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 ALLOWED_HOSTS = [
     'mscpw.onrender.com',
@@ -39,6 +44,10 @@ ALLOWED_HOSTS = [
     '[::1]',
     '0.0.0.0',
     "*",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://mscwp.onrender.com',
 ]
 # Application definition
 
